3a0c44e4742e10597f412a0c584506bf9fcb2e0f
RASEC-PC\RASEC <cesarvalencia569.cv@gmail.com>
2017-04-17 14:28:40 -0500

    ya esta registando en la base de datos

--- VisorGisSw3/VisorGisSw3/Controllers/ControlUser.js
+++ VisorGisSw3/VisorGisSw3/Controllers/ControlUser.js
@@ -18,8 +18,11 @@ module.exports = {
 
 	postregistrar: function (req, res, next){
 		console.log(req.body);
-		//var salt=bcrypt.genSaltSync(10);
-		//var pass=bcrypt.hasdSync(req.body.pas, salt);
+		bcrypt.genSalt(10, function(err, salt) {
+    	bcrypt.hash("B4c0/\/", salt, function(err, hash) {
+        // Store hash in your password DB.
+    	});
+		});
 		// console.log(req.params.id);
 		var usuario = {
 			Identificacion: req.body.ide,
@@ -33,7 +36,7 @@ module.exports = {
 		// Si, mire el mensaje que he puesto abajo, en ese caso usted pondra en vez del select un insert o uptate
 		conexion.connect();
 		conexion.connect
-		conexion.query("INSERT INTO persona (Identifiacion, Nombre, Apellido, User, Pass, email) VALUES ($1, $2, $3, $4, $5, $6)", [usuario.Identificacion, usuario.Nombre, usuario.Apellido, usuario.user, usuario.Pass, usuario.email]),
+		conexion.query("INSERT INTO persona ("/Identifiacion/", "/Nombre/", "/Apellido/", "/usuario/", "/Pass/", email) VALUES ($1, $2, $3, $4, $5, $6)", [usuario.Identificacion, usuario.Nombre, usuario.Apellido, usuario.user, usuario.Pass, usuario.email]),
 		function(err, rows, fields) {
 			if (err) {
 				return console.error('error running query', err);
@@ -46,6 +49,6 @@ module.exports = {
 	},
 	getinicioSec: function (req, res, next){
 
-		return res.render('user/registrar.jade');
+		return res.render('user/inicioSec.jade');
 	}
 };
